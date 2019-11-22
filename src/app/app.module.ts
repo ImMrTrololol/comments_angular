ec81639578d0f6ea0d449050de1172c3a2e93711
Ihor Kohut <immrtrololol@gmail.com>
2019-11-22 11:46:30 +0200

    Done!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { CommentListComponent } from './comment-list/comment-list.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: CommentListComponent },
+    ])
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    CommentListComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
